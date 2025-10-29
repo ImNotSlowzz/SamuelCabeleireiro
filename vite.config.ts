8047622db43895b12ab9b17f62d57fb334ba44f3
ImNotSlowz <thomevictorr@gmail.com>
2025-10-29 14:00:42 -0300

    Corrige configuração base do Vite para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/SamuelCabeleireiro/', // 👈 nome exato do seu repositório no GitHub
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
