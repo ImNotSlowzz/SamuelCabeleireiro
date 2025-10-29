6cf75763f423abca11d1c26d4e27eda79d00851e
ImNotSlowz <thomevictorr@gmail.com>
2025-10-29 14:18:47 -0300

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/SamuelCabeleireiro/', // 👈 ESSA LINHA É IMPORTANTE
-});
+  base: '/SamuelCabeleireiro/', // 👈 adicione esta linha
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+})
