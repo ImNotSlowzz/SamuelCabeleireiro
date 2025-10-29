ac4e888243361e03d7cb5aca3499097a25250947
ImNotSlowz <thomevictorr@gmail.com>
2025-10-29 14:07:38 -0300

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/SamuelCabeleireiro/', // 👈 nome exato do seu repositório no GitHub
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  base: '/SamuelCabeleireiro/', // 👈 ESSA LINHA É IMPORTANTE
 });
